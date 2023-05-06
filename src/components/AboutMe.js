fffcaebda88890e8249e3d3699f2d77b9a70fdf9
Ervin543 <ervindasratt@gmail.com>
2023-05-05 22:47:55 -0500

    added code to the AboutMe.js file

---
+++ src/components/AboutMe.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function AboutMe() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <img src="path/to/image" alt="Developer Name" />
+      <p>Short bio about the developer.</p>
+    </section>
+  );
+}
+
+export default AboutMe;
