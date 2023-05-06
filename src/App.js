1140402e8a41d71a742dc805f2b1baaf08172276
Ervin543 <ervindasratt@gmail.com>
2023-05-05 23:06:52 -0500

    added code to the App.js file

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./components/Header";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <About />
+        <Portfolio />
+        <Contact />
+        <Resume />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
